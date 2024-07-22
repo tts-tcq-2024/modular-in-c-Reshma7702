aa6b94aebc2730ea16eb07004fa2f76f1adff0ad
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-22 18:51:56 +0530

    Create color_data.c

---
+++ color_data.c
@@ -0,0 +1,15 @@
+#include "color_conversion.h"
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
