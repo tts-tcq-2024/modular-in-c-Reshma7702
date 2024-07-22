2fbcb9f9a76d0207fbc4808b0b41cd35e7f2c18a
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-22 18:54:27 +0530

    Update color_conversion.h

--- color_conversion.h
+++ color_conversion.h
@@ -18,9 +18,7 @@ typedef struct {
 } ColorPair;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
-
 ColorPair GetColorFromPairNumber(int pairNumber);
-
 int GetPairNumberFromColor(const ColorPair* colorPair);
 
 #endif
